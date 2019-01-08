de9f4b392c679dd780d4b811c782241b8dd760b8
saba <saba>
2019-01-08 13:08:34 -0800

    user preference model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var bcrypt = require("bcrypt-nodejs");
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
     email: {
@@ -18,17 +18,17 @@ module.exports = function(sequelize, DataTypes) {
     }
   });
 
-//   User.associate = function(models) {
-//     models.User.hasMany(models.UserData);
-//   };
+  User.associate = function (models) {
+    models.User.hasMany(models.Preference);
+  };
 
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.hook("beforeCreate", function(user) {
+  User.hook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
